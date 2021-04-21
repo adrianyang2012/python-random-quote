b35bddef456557358c4446ef7262dd654f651314
adrianyang2012 <69127002+adrianyang2012@users.noreply.github.com>
2021-04-21 14:51:16 -0700

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,4 +1,4 @@
-def main():
+def get_quotes():
    print("Keep it logically awesome.")
 
   f = open("quotes.txt")
@@ -8,4 +8,4 @@ def main():
   #print(quotes)
 
 if __name__== "__main__":
-  main()
+  get_quotes()
